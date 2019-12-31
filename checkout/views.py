35b03ce23c1f98837119770b334f966f59ac040d
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-31 22:55:02 +0000

    added contents to checkout/views.py for customer payment handling using stripe.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import MakePaymentForm, OrderForm
+from .models import OrderLineItem
+from django.conf import settings
+from django.utils import timezone
+from products.models import Product
+import stripe
 
-# Create your views here.
+# api for stripe, gets secret_key from settings
+stripe.api_key = settings.STRIPE_SECRET
+
+
+@login_required()
+def checkout(request):
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
+
+        # order saved in real time if correct information on order and payment forms
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+
+            cart = request.session.get('cart', {})
+            # initialising total of 0, then do for loop.
+            # currency is 0 and can be changed for whatever country you are in
+            total = 0
+            for id, quantity in cart.items():
+                # this will get quantity of product being purchased
+                product = get_object_or_404(Product, pk=id)
+                total += quantity * product.price
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity
+                )
+                order_line_item.save()
+                # creating customer charge with in-built sripe api
+            try:
+                customer = stripe.Charge.create(
+                    # strip using cents or pennies (so 10 euro = 1000 cent)
+                    amount=int(total * 100),
+                    currency="EUR",
+                    # can see who payment came from in dashboard
+                    description=request.user.email,
+                    # user id hidden from customer
+                    card=payment_form.cleaned_data['stripe_id']
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('products'))
+            # error message if not paid
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(
+                request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
+        # will all be printed on customer copy
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
