4ae984e19ea2ff69303f5356a555cb389e985905
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-31 00:28:46 +0000

    added Styling to app. Added new font-awesome folder, added images to media, changed static/syles.css to custom.css, changed templates/base.html,settings.py, contexts.py, urls.py, static/css/custom.css, temmplates/cart.html, accounts (registration.html, login.html)

--- cart/contexts.py
+++ cart/contexts.py
@@ -14,7 +14,8 @@ def cart_contents(request):
     every page
     """
     cart = request.session.get('cart', {})
-# initialising
+
+    # initialising
     cart_items = []
     total = 0
     product_count = 0
@@ -23,7 +24,6 @@ def cart_contents(request):
         product = get_object_or_404(Product, pk=id)
         total += quantity * product.price
         product_count += quantity
-        # will always know unique id
         cart_items.append({'id': id, 'quantity': quantity, 'product': product})
-        # key values pairs
+
     return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
