acaafcc6095b2d65b544be29aea05465bb0c8419
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-31 03:46:28 +0000

    changed settings.py (added env var for stripe).

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -139,5 +139,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
 # to fix an issue if using cloud9
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
