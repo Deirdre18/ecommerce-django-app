78dfb464896d872c98b5a2a189a202cf6c492d7c
Deirdre18 <deirdreweldon18@gmail.com>
2020-01-13 01:55:07 +0000

    added test_forms.py, test_models.py, test_views.py and media/test_images/error in checkout tests.png

---
+++ checkout/test_models.py
@@ -0,0 +1,21 @@
+# Got this code from https://github.com/Code-Institute-Submissions/django-tech-ecommerce-app
+
+from django.test import TestCase
+from .models import Order
+
+# Create your tests here.
+
+
+class OrderTests(TestCase):
+
+    def test_date_str_test(self):
+        test_order = Order(full_name="test",
+                           phone_number="test",
+                           country="test",
+                           postcode="test",
+                           town_or_city="test",
+                           street_address1="test",
+                           street_address2="test",
+                           date="2020-10-10")
+
+        self.assertEqual(str(test_order.date), "2020-10-10")
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    DATABASES = {'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'))}
+
 else:
     print("Postgres URL not found, using sqlite instead")
     DATABASES = {
